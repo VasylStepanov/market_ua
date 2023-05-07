3711d9f132bdcd8252ee548623b3d23e54c6a20b
VasylStepanov <vasylstepanov.v@gmail.com>
2023-05-07 16:25:22 +0300

    Back-end and Front-end backbones

---
+++ back_end/src/main/java/com/application/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package com.application.dto;
+
+public class UserDTO {
+}
---
+++ back_end/src/main/java/com/application/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.application.service.impl;
+
+import com.application.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
